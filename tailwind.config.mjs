ecedb2ed874e1da4128b64c2dff381a66bdc3677
ameunoia <argasatya16@gmail.com>
2024-04-04 10:10:31 +0700

    feat: add about pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,22 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      circle: "circle",
+    },
     extend: {
       colors: {
         primary: "#393646",
         secondary: "#F4EEE0",
+        tertiary: "#6D5D6E",
+      },
+      fontFamily: {
+        martel: ["martel", "sans-serif"],
+        trispace: ["trispace", "sans-serif"],
       },
     },
   },
